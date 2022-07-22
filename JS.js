c2231aacff88a9132d0f744cbbcb9ac00006c98b
LuisGuerra <lhguerra9405@hotmail.com>
2022-07-21 20:09:44 -0600

    ses 1 y 2

---
+++ JS.js
@@ -0,0 +1,73 @@
+//Ejercicio 1  sesion 2
+
+/* const time = 13;
+    let greeting;
+
+    if (time < 12) {
+        greeting = 'Buenos dias!'
+    } else if (time < 19) {
+        greeting = 'Buenas tardes!'
+    } else if (time >= 19 && time <=24) {
+        greeting = 'Buenas noches!'
+    } else
+    { greeting="poner tiempo válido"}
+
+    console.log(greeting); */
+
+    //ejercicio 2
+
+    /* const day= 2;
+    let text;
+
+    switch (day){
+        case 1:
+            text = 'Monday'
+            break;
+
+            case 2:
+            text = 'Tueday'
+            break;
+            
+            case 3:
+            text = 'Wenesday'
+            break;
+
+            case 4:
+            text = 'Thursday'
+            break;
+
+            case 5:
+            text = 'Friday'
+            break;
+
+            case 6:
+            text = 'Saturday'
+            break;
+
+            case 7:
+            text = 'Sunday'
+            break;
+
+         default:
+            text= 'valor iinválido'
+            break;   
+    }
+
+    console.log(text) */
+
+
+    //ejemplo de operador ternario
+
+    /* const speed= 120;
+    let message;
+
+    message = speed > 100 ? 'vas muy rapido' : 'vas a la vel correcta;'
+   /*  if( speed >100){
+        message ='vas muy rápido'
+    }else {
+        message= 'vas a la vel correcta'
+    } */
+
+    /* const isFast = speed > 100 ? true : false;
+    console.log(message)
+    console.log('en verdad vas muy rapido?', isFast) 
